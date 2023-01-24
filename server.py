82187fe5f1fa664fb6544cd550d8261ef753f3e0
AbhilashDatta <abhilashdattathegreat@gmail.com>
2023-01-24 14:17:32 +0000

    create_topic

--- server.py
+++ server.py
@@ -12,3 +12,14 @@ def index():
     return "<h1>Welcome to the Distributed Server!</h1>"
 
 
+@app.route("/topics", methods=["POST"])
+def create_topic():
+    topic_name = request.json["topic_name"]
+    
+    if topic_name in topics:
+        return jsonify({"status": "failure", "message": f"Topic '{topic_name}' already exists"})
+    
+    topics[topic_name] = []
+    log_queue[topic_name] = []
+    
+    return jsonify({"status": "success", "message": f"Topic '{topic_name}' created successfully"})
