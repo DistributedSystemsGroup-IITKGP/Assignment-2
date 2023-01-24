9160f01d3889c1c7ac67a75503de7b76c7079bc3
AbhilashDatta <abhilashdattathegreat@gmail.com>
2023-01-24 14:18:10 +0000

    list and register

--- server.py
+++ server.py
@@ -23,3 +23,37 @@ def create_topic():
     log_queue[topic_name] = []
     
     return jsonify({"status": "success", "message": f"Topic '{topic_name}' created successfully"})
+
+@app.route("/topics", methods=["GET"])
+def list_topics():
+    return jsonify({"status": "success", "topics": list(topics.keys())})
+
+
+@app.route("/consumer/register", methods=["POST"])
+def register_consumer():
+    topic_name = request.json["topic_name"]
+    
+    if topic_name not in topics:
+        return jsonify({"status": "failure", "message": f"Topic '{topic_name}' does not exist"})
+    
+    consumer_id = len(topics[topic_name])
+    topics[topic_name].append(consumer_id)
+    consumers[consumer_id] = topic_name
+    
+    return jsonify({"status": "success", "consumer_id": consumer_id})
+
+
+@app.route("/producer/register", methods=["POST"])
+def register_producer():
+    topic_name = request.json["topic_name"]
+    
+    if topic_name not in topics:
+        topics[topic_name] = []
+        log_queue[topic_name] = []
+    
+    producer_id = len(topics[topic_name])
+    topics[topic_name].append(producer_id)
+    producers[producer_id] = topic_name
+    
+    return jsonify({"status": "success", "producer_id": producer_id})
+
