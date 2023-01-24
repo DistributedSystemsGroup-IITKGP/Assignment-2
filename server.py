92cfa419fbd9f4d4912a0e3129ef9c44db228856
AbhilashDatta <abhilashdattathegreat@gmail.com>
2023-01-24 14:18:38 +0000

    produce-consume

--- server.py
+++ server.py
@@ -57,3 +57,53 @@ def register_producer():
     
     return jsonify({"status": "success", "producer_id": producer_id})
 
+
+@app.route("/producer/produce", methods=["POST"])
+def enqueue():
+    topic_name = request.json["topic_name"]
+    producer_id = request.json["producer_id"]
+    log_message = request.json["log_message"]
+    
+    if topic_name not in topics:
+        return jsonify({"status": "failure", "message": f"Topic '{topic_name}' does not exist"})
+    
+    if producer_id not in producers or producers[producer_id] != topic_name:
+        return jsonify({"status": "failure", "message": "Invalid producer_id"})
+    
+    log_queue[topic_name].append(log_message)
+    
+    return jsonify({"status": "success"})
+
+
+@app.route("/consumer/consume", methods=["GET"])
+def dequeue():
+    topic_name = request.json["topic_name"]
+    consumer_id = request.json["consumer_id"]
+    
+    if topic_name not in topics:
+        return jsonify({"status": "failure", "message": f"Topic '{topic_name}' does not exist"})
+    
+    if consumer_id not in consumers or consumers[consumer_id] != topic_name:
+        return jsonify({"status": "failure", "message": "Invalid consumer_id"})
+    
+    if not log_queue[topic_name]:
+        return jsonify({"status": "failure", "message": "Queue is empty"})
+    
+    log_message = log_queue[topic_name].pop(0)
+    
+    return jsonify({"status": "success", "log_message": log_message})
+
+
+@app.route("/size", methods=["GET"])
+def size():
+    topic_name = request.json["topic_name"]
+    consumer_id = request.json["consumer_id"]
+
+    if topic_name not in topics:
+        return jsonify({"status": "failure", "message": f"Topic '{topic_name}' does not exist"})
+    
+    if consumer_id not in consumers or consumers[consumer_id] != topic_name:
+        return jsonify({"status": "failure", "message": "Invalid consumer_id"})
+    
+    return jsonify({"status": "success", "size": len(log_queue[topic_name])})
+
