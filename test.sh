7b9707a01e93ce9572393db52138faf669cf6f55
AbhilashDatta <abhilashdattathegreat@gmail.com>
2023-01-26 07:16:09 +0000

    new tests

--- test.sh
+++ test.sh
@@ -6,8 +6,10 @@ curl -X POST -H "Content-Type: application/json" -d '{"topic_name":"example_topi
 
 curl -X POST -H "Content-Type: application/json" -d '{"topic_name":"example_topic"}' http://localhost:5000/producer/register
 
-curl -X POST -H "Content-Type: application/json" -d '{"topic_name":"example_topic", "producer_id":1, "log_message":"example log message"}' http://localhost:5000/producer/produce
+curl -X POST -H "Content-Type: application/json" -d '{"topic_name":"example_topic", "producer_id":"P_T0#0", "log_message":"example log message 1"}' http://localhost:5000/producer/produce
 
-curl -X GET -H "Content-Type: application/json" -d '{"topic_name":"example_topic", "consumer_id":0}' http://localhost:5000/consumer/consume
+curl -X POST -H "Content-Type: application/json" -d '{"topic_name":"example_topic", "producer_id":"P_T0#0", "log_message":"example log message 2"}' http://localhost:5000/producer/produce
 
-curl -X GET -H "Content-Type: application/json" -d '{"topic_name":"example_topic", "consumer_id":0}' http://localhost:5000/size
+curl -X GET -H "Content-Type: application/json" -d '{"topic_name":"example_topic", "consumer_id":"C_T0#0"}' http://localhost:5000/consumer/consume
+
+curl -X GET -H "Content-Type: application/json" -d '{"topic_name":"example_topic", "consumer_id":"C_T0#0"}' http://localhost:5000/size
