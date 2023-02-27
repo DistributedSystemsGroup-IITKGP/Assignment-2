a5d5133519935181f0a1b5f8d08c12f818f0c69d
sunanda690 <sunanda1sripally@gmail.com>
2023-02-28 04:28:59 +0530

    scanning fixed

--- part2/health_check.py
+++ part2/health_check.py
@@ -1,23 +1,36 @@
-import subprocess
+import ipaddress
+import multiprocessing
+import threading
+import time
 import requests
 
-def get_list_of_brokers():
-	port = '9000'
-	p = subprocess.run(["arp", "-a"],  check=True, stdout=subprocess.PIPE)
-	out =  p.stdout.decode('UTF-8')
+port = '9000'
+def scan(ip):
+    try:
+        response = requests.get(f'http://{ip}:{port}/status', timeout=0.01)
+        if response.status_code == 200 and response.json()["message"] == "broker running":
+           return ip
+    except:
+       pass
 
-	# Split the output into lines and parse each line to extract IP addresses
-	ip_addresses = []
-	for line in out.splitlines():
-		ip_address = line.split("(")[1].split(")")[0]
-		try:
-			print(ip_address)
-			response = requests.get(f'http://{ip_address}:{port}/status', timeout=0.01)
-			
-			if response.status_code == 200 and response.json()["message"] == "broker running":
-				ip_addresses.append(ip_address)
-			print('successful')
-		except:
-			continue
 
-	return ip_addresses
+def scan_network():
+    ip_range = '10.145.128.0/17'
+    pool = multiprocessing.Pool(processes=50)
+    ips = [f"{ip}" for ip in ipaddress.IPv4Network(ip_range)]
+    results = pool.map(scan, ips)
+    active_ips = [ip for ip in results if ip is not None]
+    return active_ips
+
+def do():
+    ips = scan_network()
+    print(ips)
+
+    
+def run_thread():
+    while True:
+        do()
+        time.sleep(10)
+
+if __name__ == '__main__':
+    run_thread()
